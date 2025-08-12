2626e9fa758ebd87b2170170cc6ee4df12ac1e59
Claudio <claudio.marino@outlook.com>
2025-08-12 17:15:00 +0200

    ADD AUTH CONTEXT PROVIDER FOR USER AUTHENTICATION

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { supabase } from '../autoBarrell'
+
+const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+	const [user, setUser] = useState([])
+
+	useEffect(() => {
+		const { data: authListener } = supabase.auth.onAuthStateChange(
+			(event, session) => {
+				;async (event, session) => {
+					console.log(event, session)
+					if (session?.user === null) {
+						setUser(null)
+					} else {
+						setUser(session?.user)
+					}
+				}
+			}
+		)
+
+		return () => {
+			authListener.subscription
+		}
+	}, [])
+}
